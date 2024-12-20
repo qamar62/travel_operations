0d5366492748d7f4466ca4c9c6428f9a91a1b4ce
qamar62 <qam600@gmail.com>
2024-12-20 16:14:10 +0400

    Update passenger_wsgi.py

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,4 +1,16 @@
 import os
 import sys
 
-from core.wsgi import application
+# Add the project directory to the Python path
+INTERP = os.path.expanduser("/opt/alt/python39/venv/bin/python")
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+
+# Set up Django's settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Import the WSGI application
+from core.wsgi import application  # This should be after setting DJANGO_SETTINGS_MODULE
