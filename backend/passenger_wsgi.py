2ae723886526067d4a8b1ae25a71d6ba88523200
qamar62 <qam600@gmail.com>
2024-12-20 16:16:48 +0400

    Update passenger_wsgi.py

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,16 +1,18 @@
 import os
 import sys
 
-# Add the project directory to the Python path
-INTERP = os.path.expanduser("/opt/alt/python39/venv/bin/python")
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
+# Add your virtualenv Python path
+VIRTUALENV = "/home/yy89pc8fin0s/virtualenv/travel_operations/backend/3.9/lib/python3.9/site-packages"
+if VIRTUALENV not in sys.path:
+    sys.path.insert(0, VIRTUALENV)
 
-cwd = os.getcwd()
-sys.path.append(cwd)
+# Add the project directory to the Python path
+project_dir = "/home/yy89pc8fin0s/travel_operations/backend"
+if project_dir not in sys.path:
+    sys.path.insert(0, project_dir)
 
 # Set up Django's settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 # Import the WSGI application
-from core.wsgi import application  # This should be after setting DJANGO_SETTINGS_MODULE
+from core.wsgi import application
