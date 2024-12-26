a636a6cb44cbd327e72c83301b3b92eae6f27a35
qamar62 <qam600@gmail.com>
2024-12-26 13:30:24 +0400

    Add hotelVoucher component files

---
+++ frontend/src/components/hotelVoucher/index.ts
@@ -0,0 +1,5 @@
+export { default as HotelVoucherList } from './HotelVoucherList.tsx';
+export { default as HotelVoucherDetail } from './HotelVoucherDetail.tsx';
+export { default as HotelVoucherCreate } from './HotelVoucherCreate.tsx';
+export { default as HotelVoucherEdit } from './HotelVoucherEdit.tsx';
+export { default as HotelVoucherForm } from './HotelVoucherForm.tsx';
